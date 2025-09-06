99be42a43dfbbb9b4c2faf26bbb5a19dba5c31df
Francisco De La Paz <fdelapazjr85668@uat.edu>
2025-09-06 05:12:30 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,7 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
 export default defineConfig({
   plugins: [react()],
-  base: '/ReactTutorial/',
+  base: '/ReactTutorial/',   // must match the repo’s name exactly
 })
